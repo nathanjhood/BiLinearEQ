38c9641f7a68e8d0a79c0e95a5404ae6c2f08e90
Nathan Hood <nathanjhood@googlemail.com>
2022-06-14 22:05:36 +0200

    Added params for all filters

--- Source/PluginParameters.cpp
+++ Source/PluginParameters.cpp
@@ -17,18 +17,14 @@ Parameters::Parameters(BiLinearEQAudioProcessor& p, APVTS& apvts) : audioProcess
 
 void Parameters::setParameterLayout(Params& params)
 {
-    const auto dBMax = juce::Decibels::gainToDecibels(16.0f);
-    const auto dBMin = juce::Decibels::gainToDecibels(0.0625f);
+    const auto dBMax = juce::Decibels::gainToDecibels(32.0f);
+    const auto dBMin = juce::Decibels::gainToDecibels(0.03125f);
     const auto dBOut = juce::Decibels::gainToDecibels(0.5f, -120.0f) * 20.0f;
 
     const auto freqRange = juce::NormalisableRange<float>(20.00f, 20000.00f, 0.001f, 00.198894f);
-    const auto resRange = juce::NormalisableRange<float>(00.00f, 1.00f, 0.01f, 1.00f);
     const auto gainRange = juce::NormalisableRange<float>(dBMin, dBMax, 0.01f, 1.00f);
     const auto mixRange = juce::NormalisableRange<float>(00.00f, 100.00f, 0.01f, 1.00f);
     const auto outputRange = juce::NormalisableRange<float>(dBOut, dBMax, 0.01f, 1.00f);
-
-    const auto fString = juce::StringArray({ "LP", "HP", "LS", "LSc", "HS", "HSc" });
-    const auto tString = juce::StringArray({ "DFI", "DFII", "DFI t", "DFII t" });
 
     const auto decibels = juce::String { ( "dB" ) };
     const auto frequency = juce::String { ( "Hz" ) };
@@ -46,21 +42,24 @@ void Parameters::setParameterLayout(Params& params)
 
     params.add
         //======================================================================
-        (std::make_unique<juce::AudioProcessorParameterGroup>("masterID", "0", "seperator0",
+        (std::make_unique<juce::AudioProcessorParameterGroup>("filtersID", "0", "seperator0",
             //==================================================================
-            std::make_unique<juce::AudioParameterChoice>("transformID", "Transform", tString, 3),
-            std::make_unique<juce::AudioParameterFloat>("outputID", "Output", outputRange, 00.00f, decibels, genParam),
-            std::make_unique<juce::AudioParameterFloat>("mixID", "Mix", mixRange, 100.00f, percentage, genParam),
-            std::make_unique<juce::AudioParameterBool>("bypassID", "Bypass", false)
+            std::make_unique<juce::AudioParameterFloat>("hpFrequencyID", "HP", freqRange, 632.455f, frequency, genParam),
+            std::make_unique<juce::AudioParameterFloat>("lsFrequencyID", "LS", freqRange, 20.00f, frequency, genParam),
+            std::make_unique<juce::AudioParameterFloat>("lsGainID", "dB", gainRange, 0.0f, decibels, genParam),
+            std::make_unique<juce::AudioParameterFloat>("hsFrequencyID", "HS", freqRange, 20000.00f, frequency, genParam),
+            std::make_unique<juce::AudioParameterFloat>("hsGainID", "dB", gainRange, 0.0f, decibels, genParam),
+            std::make_unique<juce::AudioParameterFloat>("lpFrequencyID", "LP", freqRange, 632.455f, frequency, genParam)
             //==================================================================
             ));
 
     params.add
         //======================================================================
-        (std::make_unique<juce::AudioProcessorParameterGroup>("filtersID", "1", "seperator1",
+        (std::make_unique<juce::AudioProcessorParameterGroup>("masterID", "1", "seperator1",
             //==================================================================
-            std::make_unique<juce::AudioParameterFloat>("hpFrequencyID", "HP", freqRange, 632.455f, frequency, genParam),
-            std::make_unique<juce::AudioParameterFloat>("lpFrequencyID", "LP", freqRange, 632.455f, frequency, genParam)
+            std::make_unique<juce::AudioParameterFloat>("outputID", "Output", outputRange, 00.00f, decibels, genParam),
+            std::make_unique<juce::AudioParameterFloat>("mixID", "Mix", mixRange, 100.00f, percentage, genParam),
+            std::make_unique<juce::AudioParameterBool>("bypassID", "Bypass", false)
             //==================================================================
             ));
 }
